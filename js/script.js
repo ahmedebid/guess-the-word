7890edc8310d252b73f60823d4bdfe5280403015
ahmedebid <ahmed-ebid@outlook.com>
2022-03-24 13:30:34 +0200

    Part 2 of the project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // The unordered list where the player’s guessed letters will appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 // The "Guess" button
 const guessButton = document.querySelector(".guess");
 // The text input where the player will guess a letter
@@ -14,9 +14,10 @@ const remainingGuessesNumber = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 // The hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again");
-
 // The starting word for the game
 const word = "magnolia";
+// An array to contain all the letters that the player guesses
+const guessedLetters = [];
 
 // A function to add a circle symbol in place of each letter in the "word in progress"
 const updateWordInProgress = function (word) {
@@ -25,7 +26,7 @@ const updateWordInProgress = function (word) {
         circles.push("●");
     }
     wordInProgress.innerText = circles.join("");
-}
+};
 
 updateWordInProgress(word);
 
@@ -35,7 +36,46 @@ guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     // capturing the user input
     const userInput = input.value;
-    console.log(userInput);
+    //console.log(userInput);
     // emptying the input field
     input.value = "";
-})
+
+    // emptying the text of the message element
+    message.innerText = "";
+
+    // call the function to check the input
+    const validatedInput = validatePlayerInput(userInput);
+
+    console.log(validatedInput);
+
+    if (validatedInput !== undefined) {
+        makeGuess(validatedInput);
+    }
+});
+
+// Validate the user input (check if it's not a letter, more than one character or empty)
+const validatePlayerInput = function (userInput) {
+    // A regular expression to ensure the player inputs a letter
+    const acceptedLetter = /[a-zA-Z]/
+    if (userInput === "") {
+        message.innerText = "You didn't enter any letters!";
+    } else if (userInput.length > 1) {
+        message.innerText = "Please enter 1 character only";
+    } else if (!userInput.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z";
+    } else {
+        return userInput;
+    }
+};
+
+// Capture user input
+const makeGuess = function (letter) {
+    const upperCaseLetter = letter.toUpperCase();
+    if (guessedLetters.includes(upperCaseLetter)) {
+        message.innerText = "You guessed this letter before ... Guess another one!"
+    } else {
+        guessedLetters.push(upperCaseLetter);
+        console.log(guessedLetters);
+    }
+};
+
